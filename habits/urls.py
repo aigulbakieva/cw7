a4b7c43ecd3c11d1d2447a28be0aafb92ba2029b
aigulbakieva <bakisha.a@mail.ru>
2024-05-18 23:46:00 +0100

    валидаторы

--- habits/urls.py
+++ habits/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from habits.apps import HabitsConfig
 from habits.views import HabitCreateApiView, HabitListApiView, HabitRetrieveApiView, HabitDestroyApiView, \
-    HabitUpdateApiView
+    HabitUpdateApiView, PublishHabitListApiView
 
 app_name = HabitsConfig.name
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path("create/", HabitCreateApiView.as_view(), name='habit-create'),
     path("delete/<int:pk>/", HabitDestroyApiView.as_view(), name='habit-delete'),
     path("update/<int:pk>/", HabitUpdateApiView.as_view(), name='habit-update'),
+    path("published/", PublishHabitListApiView.as_view(), name='habit-publish'),
 ]
