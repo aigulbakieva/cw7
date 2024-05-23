8e7091a25088b6c9dc5026be3cc65decad83794f
aigulbakieva <bakisha.a@mail.ru>
2024-05-24 00:47:14 +0100

    tests

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from habits.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email="test@test.pro",
+            password="123qwe",
+            is_active=True,
+            is_staff=True,
+        )
+        self.habit = Habit.objects.create(
+            owner=self.user,
+            action="Читать книгу",
+            place="дом",
+            is_nice=False,
+        )
